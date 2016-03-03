7dcf4d44de7f9199c9aa87d71437f442c04327d8
kuolun <kuolun@gmail.com>
2016-03-03 18:43:58 +0800

    bug fix

--- server/server.js
+++ server/server.js
@@ -1,19 +1,21 @@
 var express = require('express');
 var app = express();
 
-// app.get('/', function(req, res) {
-//     res.send('hello express');
-// });
+var PORT = process.env.PORT || 30000;
+app.get('/', function(req, res) {
+    res.send('hello express');
+});
 
 app.get('/about', function(req, res) {
     res.send('About us page');
 });
 
 
-console.log(__dirname + '\\public');
-app.use(express.static(__dirname + '\\public'));
+// console.log(__dirname + '\\public');
+// app.use(express.static(__dirname + '\\public'));
 
 
-app.listen(30000, function() {
+
+app.listen(PORT, function() {
     console.log("server working");
 });
