edd0ddfe798ca1a532d3275092ae687c8c3c237c
kuolun <kuolun@gmail.com>
2016-03-03 18:04:42 +0800

    first commit

---
+++ server/server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+
+// app.get('/', function(req, res) {
+//     res.send('hello express');
+// });
+
+app.get('/about', function(req, res) {
+    res.send('About us page');
+});
+
+
+console.log(__dirname + '\\public');
+app.use(express.static(__dirname + '\\public'));
+
+
+app.listen(30000, function() {
+    console.log("server working");
+});
